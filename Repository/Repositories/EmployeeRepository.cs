1d672a03793c0f311ebaa072d3db10383d7245d5
oleg <olegfide@mail,ru>
2023-05-08 23:44:00 +0500

    supressed issue

--- Repository/Repositories/EmployeeRepository.cs
+++ Repository/Repositories/EmployeeRepository.cs
@@ -19,8 +19,8 @@ public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
         var employees = await FindByCondition(e => e.CompanyId.Equals(companyId),
                 trackChanges)
             .FilterEmployees(employeeParameters.MinAge, employeeParameters.MaxAge)
-            .Search(searchTerm: employeeParameters.SearchTerm)
-            .Sort(employeeParameters.OrderBy)
+            .Search(searchTerm: employeeParameters.SearchTerm!)
+            .Sort(employeeParameters.OrderBy!)
             .ToListAsync();
 
         var count = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
