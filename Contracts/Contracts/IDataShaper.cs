0d021210e622c4cc0ccc765d84020d39c73df3f9
oleg <olegfide@mail,ru>
2023-05-17 11:59:05 +0500

    got rif of code isuues

--- Contracts/Contracts/IDataShaper.cs
+++ Contracts/Contracts/IDataShaper.cs
@@ -7,5 +7,6 @@ public interface IDataShaper<in T>
     IEnumerable<ExpandoObject> ShapeData(IEnumerable<T> entities, string?
         fieldsString);
 
+    // ReSharper disable once UnusedMember.Global
     ExpandoObject ShapeData(T entity, string? fieldsString);
 }
---
+++ SqlQueries/SQLQuery1.sql
@@ -0,0 +1,2 @@
+﻿select * from employees
+where CompanyId='c9d4c053-49b6-410c-bc78-2d54a9991870'
