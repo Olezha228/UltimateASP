fdf5ea64e20610ce497bc6d5e44c73bf2ea2d937
oleg <olegfide@mail,ru>
2023-07-28 21:57:22 +0500

    jwt

---
+++ Service/AuthenticationService.cs
@@ -0,0 +1,117 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using AutoMapper;
+using Entities.Models;
+using LoggerService;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Service.Contracts.ServiceInterfaces;
+using Shared.DataTransferObjects;
+
+namespace Service;
+
+internal sealed class AuthenticationService : IAuthenticationService
+{
+    private readonly ILoggerManager _logger;
+    private readonly IMapper _mapper;
+    private readonly UserManager<User> _userManager;
+    private readonly IConfiguration _configuration;
+
+    private User? _user;
+
+    public AuthenticationService(ILoggerManager logger, IMapper mapper,
+        UserManager<User> userManager, IConfiguration configuration)
+    {
+        _logger = logger;
+        _mapper = mapper;
+        _userManager = userManager;
+        _configuration = configuration;
+    }
+
+    public async Task<IdentityResult> RegisterUser(UserForRegistrationDto userForRegistration)
+    {
+        var user = _mapper.Map<User>(userForRegistration);
+
+        var result = await _userManager.CreateAsync(user,
+            userForRegistration.Password);
+
+        //If you want, before calling AddToRoleAsync or AddToRolesAsync, you
+        //can check if roles exist in the database. But for that, you have to inject
+        //RoleManager < TRole > and use the RoleExistsAsync method.
+
+
+        if (result.Succeeded)
+            await _userManager.AddToRolesAsync(user, userForRegistration.Roles);
+
+        return result;
+    }
+
+    public async Task<bool> ValidateUser(UserForAuthenticationDto userForAuth)
+    {
+        _user = await _userManager.FindByNameAsync(userForAuth.UserName!);
+
+        var isResultValid = IsPasswordMatches(userForAuth);
+
+        if (!isResultValid.Result)
+            _logger.LogWarn(
+                $"{nameof(ValidateUser)}: Authentication failed. Wrong user name or password.");
+
+        return isResultValid.Result;
+    }
+
+    private async Task<bool> IsPasswordMatches(UserForAuthenticationDto userForAuth)
+    {
+        return (_user != null) && await _userManager.CheckPasswordAsync(_user, userForAuth.Password!);
+    }
+
+    public async Task<string> CreateToken()
+    {
+        var signingCredentials = GetSigningCredentials();
+        var claims = await GetClaims();
+        var tokenOptions = GenerateTokenOptions(signingCredentials, claims);
+
+        return new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+    }
+
+    private static SigningCredentials GetSigningCredentials()
+    {
+        var key = Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("SECRET"));
+
+        var secret = new SymmetricSecurityKey(key);
+
+        return new SigningCredentials(secret, SecurityAlgorithms.HmacSha256);
+    }
+
+    private async Task<List<Claim>> GetClaims()
+    {
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.Name, _user.UserName)
+        };
+
+        var roles = await _userManager.GetRolesAsync(_user);
+
+        claims.AddRange(roles.Select(role => new Claim(ClaimTypes.Role, role)));
+
+        return claims;
+    }
+
+    private JwtSecurityToken GenerateTokenOptions(SigningCredentials signingCredentials,
+        IEnumerable<Claim> claims)
+    {
+        var jwtSettings = _configuration.GetSection("JwtSettings");
+
+        var tokenOptions = new JwtSecurityToken
+        (
+            issuer: jwtSettings["validIssuer"],
+            audience: jwtSettings["validAudience"],
+            claims: claims,
+            expires: DateTime.Now.AddMinutes(Convert.ToDouble(jwtSettings["expires"])),
+            signingCredentials: signingCredentials
+        );
+
+        return tokenOptions;
+    }
+}
---
+++ UltimateASP/Migrations/20230722150720_AddedRolesToDb.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace UltimateASP.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedRolesToDb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "4f8bc8ba-fea1-4113-ac32-1d0a9b5c6576", null, "Administrator", "ADMINISTRATOR" },
+                    { "ac4361c6-a116-4216-a3e1-1efff8778476", null, "Manager", "MANAGER" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "4f8bc8ba-fea1-4113-ac32-1d0a9b5c6576");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ac4361c6-a116-4216-a3e1-1efff8778476");
+        }
+    }
+}
